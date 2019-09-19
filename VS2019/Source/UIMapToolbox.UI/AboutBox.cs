72391fbe6c6b8f1e848a5bdf6e1d8e68b406cb44
n-fowler <nfowler587@gmail.com>
2019-09-19 12:53:24 -0400

    Initial check in of 2019 branch, no changes yet

---
+++ VS2019/Source/UIMapToolbox.UI/AboutBox.cs
@@ -0,0 +1,79 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace UIMapToolbox.UI
+{
+    sealed partial class AboutBox : Form
+    {
+        public AboutBox()
+        {
+            InitializeComponent();
+            Text = $@"About {Program.ApplicationName}";
+            labelProductName.Text = Program.ApplicationName;
+            labelVersion.Text = $@"Version {AssemblyVersion}";
+            textBoxDescription.Text =
+                @"UIMap Toolbox allows you to restructure, split and merge UIMaps created for Microsoft CodedUI tests in Visual Studio 2010, 2012, 2013, 2015 and 2017.";
+        }
+
+        #region Assembly Attribute Accessors
+
+        public string AssemblyTitle
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+                if (attributes.Length <= 0)
+                {
+                    return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+                }
+                AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+                return titleAttribute.Title != "" ? titleAttribute.Title : System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+            }
+        }
+
+        private static string AssemblyVersion => Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        public string AssemblyDescription
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+                return attributes.Length == 0 ? "" : ((AssemblyDescriptionAttribute)attributes[0]).Description;
+            }
+        }
+
+        public string AssemblyProduct
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+                return attributes.Length == 0 ? "" : ((AssemblyProductAttribute)attributes[0]).Product;
+            }
+        }
+
+        public string AssemblyCopyright
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+                return attributes.Length == 0 ? "" : ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+            }
+        }
+
+        public string AssemblyCompany
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+                return attributes.Length == 0 ? "" : ((AssemblyCompanyAttribute)attributes[0]).Company;
+            }
+        }
+        #endregion
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(linkLabel1.Text);
+        }
+    }
+}
---
+++ VS2019/Source/UIMapToolbox.UI/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace UIMapToolbox.UI
+{
+    static class Program
+    {
+        public const string ApplicationName = "UIMap Toolbox";
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.ThreadException += Application_ThreadException;
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+
+        static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            string msg =
+                $"An error occured. Please report to https://github.com/n-fowler/CodedUIMapToolbox with information on how to reproduce issue.\n\n{e.Exception.Message}\n\nStack trace:\n{e.Exception.StackTrace}";
+
+            MessageBox.Show(msg, ApplicationName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+}
